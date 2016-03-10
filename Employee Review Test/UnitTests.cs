47ce6fd5d815ae6d4f2c9d47a3e2c05dd0dfaa4a
Kate Ramsey <kateramsey@live.com>
2016-03-10 14:21:52 -0600

    Start building unit tests

--- Employee Review Test/UnitTests.cs
+++ Employee Review Test/UnitTests.cs
@@ -6,11 +6,19 @@ namespace Employee_Review_Test
     [TestClass]
     public class EmployeeReviewTest
     {
-
+        Department d = new Department("Sales");
+        Employee e = new Employee("Kate", "kramsey@thv11.com", "501-743-6074", 32000);
 
         [TestMethod]
-        public void TestMethod1()
+        public void DepartmentCreation()
         {
+            Assert("Sales", d.Name())
+        }
+
+        [TestMethod]
+        public void EmployeeName()
+        {
+            Assert("Kate", e.Name())
         }
     }
 }
