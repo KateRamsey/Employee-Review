fcb6f0cc146cdbe7e0289a1cffbd02dae381556e
Kate Ramsey <kateramsey@live.com>
2016-03-10 14:38:01 -0600

    adding test methods

--- Employee Review Test/UnitTests.cs
+++ Employee Review Test/UnitTests.cs
@@ -7,18 +7,39 @@ namespace Employee_Review_Test
     public class EmployeeReviewTest
     {
         Department d = new Department("Sales");
-        Employee e = new Employee("Kate", "kramsey@thv11.com", "501-743-6074", 32000);
+        Employee kate = new Employee("Kate", "kramsey@thv11.com", "501-743-6074", 8000);
 
         [TestMethod]
         public void DepartmentCreation()
         {
-            Assert("Sales", d.Name())
+            Assert.AreEqual("Sales", d.Name());
         }
 
         [TestMethod]
         public void EmployeeName()
         {
-            Assert("Kate", e.Name())
+            Assert.AreEqual("Kate", kate.Name());
         }
+
+        [TestMethod]
+        public void AddEmployeeToDepartment()
+        {
+            d.AddEmployee(kate);
+            Assert.IsFalse(d.EmployeeList.count() == 0);
+        }
+
+        [TestMethod]
+        public void GetEmployeeName()
+        {
+            string employeeName = kate.Name();
+            Assert.AreEqual("Kate", employeeName);
+        }
+        [TestMethod]
+        public void GetEmployeeSalary()
+        {
+            string employeeSalary = kate.Salary();
+            Assert.AreEqual(8000, employeeSalary);
+        }
+
     }
 }
